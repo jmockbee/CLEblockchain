8a8e1fc8113cf7c5ce611854cd93066a2808b5a4
jmockbee <mockbee1000@gmail.com>
2018-05-08 19:08:02 -0400

    testing timestamp first generation

--- block.js
+++ block.js
@@ -17,6 +17,13 @@ class Block {
         return new this ('Genesis time', '-----', 'f1r57-h45h', [] );
    
    }
+   static mineBlock (lastBlock, data ) {  
+    const timestamp = Date.now ();
+    const lastHash =lastBlock.hash;
+    const hash = 'todo-hash';
+
+    return new this(timestamp,lastHash,hash,data);
+   }
 }
 
 
--- dev-test.js
+++ dev-test.js
@@ -1,6 +1,11 @@
 const Block = require('./block');
 
+
+/* commenting this out so I can test  if block is reproducing 
 const block = new Block ('foo', 'bar', 'zoo', 'baz');
 
 console.log (block.toString());
-console.log(Block.genesis().toString());
+console.log(Block.genesis().toString()); */
+
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log (fooBlock.toString());
