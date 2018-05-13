5afaeea4b66110918cf92fd9c5a04f8a403e0ff5
jmockbee <mockbee1000@gmail.com>
2018-05-13 12:32:54 -0400

    making sure the genesis block adds new block

--- block.test.js
+++ block.test.js
@@ -1,23 +1,18 @@
 const Block = require('./block');
 
-describe ('Block', () => {
-    let data, lastBlock, block;
-
+describe('Block', () => {
+let data, lastBlock, block;
 beforeEach(() => {
-     data = 'bar';
-     lastBlock = Block.genesis();
-     block = Block.mineBlock(lastBlock,data);
+	data = 'bar';
+lastBlock = Block.genesis();
+block = Block.mineBlock(lastBlock, data);
 });
-
-    it ('sets the `data ` to match the input',() => {
-        expect(block.data).toEqual(data);
-
-    });
-
-
-    it ('sets the `lastHash` to match the has of the last block', ()=>{
-
-        expect ( block.lastHash).toEqual(lastBlock.hash);
-
-    } );
+	
+it('sets the `data` to match the input', () => {
+expect(block.data).toEqual(data);
+});
+	
+it('sets the `lastHash` to match the hash of the last block', () => {
+expect(block.lastHash).toEqual(lastBlock.hash);
+});
 });
--- blockchain.js
+++ blockchain.js
@@ -1,16 +1,15 @@
-const Block = require('./ block');
+const Block = require('./block');
 
 class Blockchain {
-constructor() {
-    this.chain  = [Block.genesis()];
-}
-addBlock(data){
-   
-    const block = Block.mineBlock(this.chain[this.chain.length-1],data);
-this.chain.push(block);
-return block;
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
 
-}
+  addBlock(data) {
+    const block = Block.mineBlock(this.chain[this.chain.length-1], data);
+    this.chain.push(block);
+    return block;
+  }
 }
 
 module.exports = Blockchain;
